7683deb2cbed55e32df3a883688a31a3015a94a7
Illia-Bukhantsev <74241217+Illia-Bukhantsev@users.noreply.github.com>
2026-05-13 19:56:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -23,45 +23,37 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-// LIGHTS
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
 
-// CUBE
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 const material = new THREE.MeshStandardMaterial({
   color: 0x0077ff
 });
 
 const cube = new THREE.Mesh(geometry, material);
-
 scene.add(cube);
 
-// ANIMATION
 function animate() {
   requestAnimationFrame(animate);
 
+  cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-  cube.rotation.x += 0.005;
 
   controls.update();
-
   renderer.render(scene, camera);
 }
 
 animate();
 
-// RESIZE
 window.addEventListener('resize', () => {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-
 });
