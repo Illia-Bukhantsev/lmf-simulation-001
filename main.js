9b215d0c59c7a46a4b3e8f84062ec57a56a46eaf
Illia-Bukhantsev <74241217+Illia-Bukhantsev@users.noreply.github.com>
2026-05-13 19:47:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.154/examples
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xaaaaaa);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
 camera.position.set(3, 3, 5);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -13,20 +19,23 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// light
 const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5,5,5);
+light.position.set(5, 5, 5);
 scene.add(light);
 
-// test cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
+const material = new THREE.MeshStandardMaterial({
+  color: 0x0077ff
+});
+
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 function animate() {
   requestAnimationFrame(animate);
+
   cube.rotation.y += 0.01;
+
   controls.update();
   renderer.render(scene, camera);
 }
